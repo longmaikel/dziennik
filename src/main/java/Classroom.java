ed58486f4986d6f7bb720d7180867a3fbe8eb3b5
maikel <kutaj74@gmail.com>
2022-04-04 20:10:55 +0200

    added methods, refactoring code, show use cases in main

--- src/main/java/Classroom.java
+++ src/main/java/Classroom.java
@@ -8,24 +8,34 @@ public class Classroom {
     private List<Student> students = new ArrayList<Student>();
     private int maxStudentsCount;
     private final PrintStream output;
+    private final PrintStream error;
 
     public Classroom(String name, int maxStudentsCount) {
         this.name = name;
         this.maxStudentsCount = maxStudentsCount;
         this.output = System.out;
+        this.error = System.err;
     }
 
     public void addStudent(Student student) {
-        if (this.containsStudent(student) || this.isFull()) {
-            throw new IllegalStateException("brak miejsca");
+        if (this.isFull()) {
+            this.error.println("Klasa jest wypełniona po brzegi.");
         }
+
+        if (this.containsStudent(student)) {
+            this.error.println("Uczen zostal juz dodany do klasy.");
+        }
+
         this.students.add(student);
     }
 
     public void addPoints(Student student, double points) {
 
-        Optional<Student> studentOnList = this.students.stream().filter(el -> 0 == el.compareTo(student)).findFirst();
-        if (studentOnList.isEmpty()){
+        Optional<Student> studentOnList = this.students.stream()
+                .filter(el -> 0 == el.compareTo(student))
+                .findFirst();
+
+        if (studentOnList.isEmpty()) {
             return;
         }
 
@@ -33,8 +43,9 @@ public class Classroom {
     }
 
     public void removePoints(Student student, double points) {
-        if (points >= 0) {
-            throw new IllegalArgumentException();
+        if (points <= 0) {
+            this.error.println("Punkty muszą być liczbą dodatnią");
+            return;
         }
 
         this.addPoints(student, points);
@@ -44,7 +55,7 @@ public class Classroom {
 
         Iterator<Student> iterator = this.students.iterator();
 
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             Student listStudent = iterator.next();
 
             if (0 == listStudent.compareTo(student) && 0 == student.getPoints()) {
@@ -58,7 +69,10 @@ public class Classroom {
     }
 
     public void changeCondition(Student student, StudentCondition state) {
-        Optional<Student> optionalStudent = this.students.stream().filter(el -> 0 == el.compareTo(student)).findFirst();
+
+        Optional<Student> optionalStudent = this.students.stream()
+                .filter(el -> 0 == el.compareTo(student))
+                .findFirst();
 
         if (optionalStudent.isEmpty()){
             return;
@@ -68,27 +82,50 @@ public class Classroom {
     }
     
     public Optional<Student> search(String lastname) {
-        return (this.students.stream().filter(el -> lastname.equals(el.getLastname()))).findFirst();
+        return this.students.stream()
+                .filter(el -> lastname.equals(el.getLastname()))
+                .findFirst();
     }
 
     public ArrayList<Student> searchPartial(String prefix) {
-        return (ArrayList<Student>) this.students.stream().filter(el -> el.getLastname().startsWith(prefix)).collect(Collectors.toList());
+        return (ArrayList<Student>) this.students.stream()
+                .filter(el -> el.getLastname().startsWith(prefix))
+                .collect(Collectors.toList());
     }
 
-    public ArrayList<Student> countByCondition(StudentCondition state) {
-        return (ArrayList<Student>) this.students.stream().filter(el -> state == el.getStatus()).collect(Collectors.toList());
+    public int countByCondition(StudentCondition state) {
+        ArrayList<Student> matchedStudents = (ArrayList<Student>) this.students.stream()
+                .filter(el -> state == el.getStatus())
+                .collect(Collectors.toList());
+        return matchedStudents.size();
     }
 
     public ArrayList<Student> sortByName() {
-        return (ArrayList<Student>) this.students.stream().sorted(Comparator.comparing(Student::getLastname)).collect(Collectors.toList());
+        return (ArrayList<Student>) this.students.stream()
+                .sorted(Comparator.comparing(Student::getLastname))
+                .collect(Collectors.toList());
     }
 
     public ArrayList<Student> sortByPoints() {
-        return (ArrayList<Student>) this.students.stream().sorted(Comparator.comparing(Student::getPoints).reversed()).collect(Collectors.toList());
+        return (ArrayList<Student>) this.students.stream()
+                .sorted(Comparator.comparing(Student::getPoints).reversed())
+                .collect(Collectors.toList());
     }
 
     public int max() {
         return this.maxStudentsCount;
+    }
+
+    public void  summary() {
+        this.output.println("----- Klasa: " + this.name + ": -----");
+        String fillInfo = "Max liczba uczniow: " +  this.maxStudentsCount + ", wypełnienie: " + this.getPercentageFilling();
+        this.output.println(fillInfo);
+        this.students.forEach(Student::print);
+        this.output.println("-------------------------------------");
+    }
+
+    public double getPercentageFilling() {
+        return (double)this.getStudentsCount() / (double)this.max() * 100.0;
     }
 
     private boolean isFull() {
@@ -100,23 +137,12 @@ public class Classroom {
     }
 
     private boolean containsStudent(Student suspicious) {
-        return this.students.stream().filter(el -> 0 == el.compareTo(suspicious)).findFirst().isPresent();
-    }
-
-
-    public double getPercentageFilling() {
-        return (double)this.getStudentsCount() / (double)this.max() * 100.0;
+        return this.students.stream()
+                .anyMatch(el -> 0 == el.compareTo(suspicious));
     }
 
     private int getStudentsCount() {
         return this.students.size();
     }
 
-    public void  summary() {
-        this.output.println("----- Klasa: " + this.name + ": -----");
-        String fillInfo = "Max liczba uczniow: " +  this.maxStudentsCount + ", wypełnienie: " + this.getPercentageFilling();
-        this.output.println(fillInfo);
-        this.students.forEach(Student::print);
-        this.output.println("-------------------------------------");
-    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,75 @@
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        final String CLASS_NAME_1 = "klasa1";
-        final String CLASS_NAME_2 = "klasa2";
-        ClassContainer classContainer = new ClassContainer();
-        classContainer.addClassroom(CLASS_NAME_1, 10);
-        classContainer.addClassroom(CLASS_NAME_2, 20);
 
-        classContainer.summary();
-        classContainer.removeClassroom(CLASS_NAME_1);
-        classContainer.summary();
+        try {
 
-        Student s1 = new Student("Michał", "Nowak", 1999, 0, StudentCondition.OK);
-        Student s2 = new Student("Jacek", "Windak", 1999, 0, StudentCondition.OK);
-        Student s3 = new Student("Anna", "Brzózka", 1999, 0, StudentCondition.OK);
-        Classroom classroom1 = classContainer.getClassroom(CLASS_NAME_2);
-        classroom1.addStudent(s1);
-        classroom1.addStudent(s2);
-        classroom1.addStudent(s3);
+            final String CLASS_NAME_1 = "klasa1";
+            final String CLASS_NAME_2 = "klasa2";
+            ClassContainer classContainer = new ClassContainer();
+            classContainer.addClassroom(CLASS_NAME_1, 2);
+            classContainer.addClassroom(CLASS_NAME_2, 2);
 
-        classroom1.summary();
+            classContainer.summary();
+            classContainer.removeClassroom(CLASS_NAME_1);
+            classContainer.summary();
+
+            Student s1 = new Student("Michał", "Nowak", 1999, 0, StudentCondition.OK);
+            Student s2 = new Student("Jacek", "Windak", 1999, 0, StudentCondition.OK);
+            Student s3 = new Student("Anna", "Brzózka", 1999, 0, StudentCondition.OK);
+            Student s4 = new Student("Anna", "Nowakowska", 1999, 0, StudentCondition.OK);
+
+            Classroom classroom1 = classContainer.getClassroom(CLASS_NAME_2);
+            classroom1.addStudent(s1);
+            classroom1.addStudent(s2);
+            classroom1.addStudent(s3);
+            classroom1.addStudent(s4);
+
+            classroom1.summary();
+
+            classroom1.addPoints(s1, 10);
+            classroom1.addPoints(s2, 5);
+            classroom1.removePoints(s3, 2);
+
+            classroom1.summary();
+
+            classroom1.changeCondition(s1, StudentCondition.ABSENT);
+
+            classroom1.summary();
+
+            Optional<Student> optionalStudent = classroom1.search("Nowak");
+
+            if (optionalStudent.isEmpty()) {
+                System.out.println("W klasie ma nie studenta o podanym nazwisku");
+            } else {
+                System.out.println("---------------------");
+                System.out.println("Znaleziony uczniowie: ");
+                optionalStudent.get().print();
+                System.out.println("---------------------");
+            }
+
+            ArrayList<Student> matchedStudents = classroom1.searchPartial("Nowa");
+            System.out.println("---------------------");
+            System.out.println("Znalezieni uczniowie: ");
+            matchedStudents.forEach(Student::print);
+            System.out.println("---------------------");
+
+            System.out.println("Liczba studentow o statusie " + StudentCondition.ABSENT + ":" + classroom1.countByCondition(StudentCondition.ABSENT));
+            System.out.println("Liczba studentow o statusie " + StudentCondition.OK + ":" + classroom1.countByCondition(StudentCondition.OK));
+
+            ArrayList<Student> sortedStudents = classroom1.sortByName();
+            System.out.println("---------------------");
+            System.out.println("posortowani uczniowie: ");
+            sortedStudents.forEach(Student::print);
+            System.out.println("---------------------");
+
+        } catch (Throwable e) {
+            System.out.println("Błąd aplikacji");
+            System.out.println(e.getMessage());
+        }
+
 
 
     }
